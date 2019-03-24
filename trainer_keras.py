71565eb9fd0efacf22089578c98751cb54c30678
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2019-03-23 22:46:26 -0300

    Added validation section to trainer

--- trainer_keras.py
+++ trainer_keras.py
@@ -64,16 +64,17 @@ print('[' + str(datetime.now()) + '] Training model...')
 sys.stdout.flush()
 df_Y = training_data.iloc[:,0]
 df_X = training_data.iloc[:,1:]
+train_X, test_X, train_y, test_y = train_test_split(df_X, df_Y, test_size = 0.10)
 y_scaler = MinMaxScaler()
-y_scaler.fit(df_Y.values.reshape(-1,1))
-df_Y = y_scaler.transform(df_Y.values.reshape(-1,1))
+y_scaler.fit(train_y.values.reshape(-1,1))
+train_y = y_scaler.transform(train_y.values.reshape(-1,1))
 x_scaler = MinMaxScaler()
-x_scaler.fit(df_X)
-df_X = x_scaler.transform(df_X)
+x_scaler.fit(train_X)
+train_X = x_scaler.transform(train_X)
 scaler = {'x':x_scaler,'y':y_scaler}
 
 ### LINES BELOW FOR KERAS DEEP NEURAL NET MODEL
-size_input = len(df_X.shape[1])
+size_input = train_X.shape[1]
 K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_parallelism_threads=6, inter_op_parallelism_threads=6)))
 model = Sequential()
 model.add(Dense(size_input, input_dim=size_input, kernel_initializer='normal', activation='relu'))
@@ -83,10 +84,9 @@ model.add(Dense(1, kernel_initializer='normal'))
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_absolute_error'])
 checkpoint_name = 'bestweights.hdf5'
 checkpoint = ModelCheckpoint(checkpoint_name, monitor='val_loss', verbose = 1, save_best_only = True, mode ='auto')
-earlystop = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=5, verbose=1, mode='auto', restore_best_weights=True)
+earlystop = EarlyStopping(monitor='val_loss', min_delta=1e-8, patience=10, verbose=1, mode='auto', restore_best_weights=True)
 callbacks_list = [checkpoint,earlystop]
-# model.fit(df_X, df_Y, epochs=500, batch_size=32, validation_split = 0.2, callbacks=callbacks_list)
-model.fit(df_X, df_Y, epochs=200, batch_size=16384, validation_split = 0.2, callbacks=callbacks_list, verbose=2)
+model.fit(train_X, train_y, epochs=500, batch_size=32768, validation_split = 0.2, callbacks=callbacks_list, verbose=2)
 print('[' + str(datetime.now()) + '] Reloading best model checkpoint...')
 model.load_weights("bestweights.hdf5")
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_absolute_error'])
@@ -105,20 +105,38 @@ try:
     model.save_weights(temp_file.name)
     s3.put(temp_file.name,weights_file)
     temp_file.close()
+    print('[' + str(datetime.now()) + '] Persisting model features...')
+    sys.stdout.flush()
     features = df_X.columns.values.tolist()
     features_file = "w210policedata/models/keras_features.pickle"
     with s3.open(features_file, "wb") as json_file:
         pickle.dump(features, json_file, protocol=pickle.HIGHEST_PROTOCOL)
         json_file.close()
+    print('[' + str(datetime.now()) + '] Persisting model scalers...')
+    sys.stdout.flush()
     scaler_file = "w210policedata/models/keras_scaler.pickle"
     with s3.open(scaler_file, "wb") as json_file:
         pickle.dump(scaler, json_file, protocol=pickle.HIGHEST_PROTOCOL)
         json_file.close()
 except:
-    print('[' + str(datetime.now()) + '] Error persisting model structure.')
+    print('[' + str(datetime.now()) + '] Error persisting model assets.')
     print('[' + str(datetime.now()) + '] Aborting...')
     sys.exit(1)
 ### END OF KERAS DEEP NEURAL NET MODEL
+
+### MODEL TESTING
+print('[' + str(datetime.now()) + '] Training complete!')
+sys.stdout.flush()
+print('[' + str(datetime.now()) + '] Running validation test...')
+sys.stdout.flush()
+test_X = scaler['x'].transform(test_X)
+predictions = model.predict(test_X)
+predictions = scaler['y'].inverse_transform(predictions)
+MAE = mean_absolute_error(test_y , predictions)
+MSE = mean_squared_error(test_y, predictions)
+print('[' + str(datetime.now()) + ']    - Keras Deep Neural Net validation MAE = ',MAE)
+print('[' + str(datetime.now()) + ']    - Keras Deep Neural Net validation MSE = ',MSE)
+print('[' + str(datetime.now()) + ']    - Keras Deep Neural Net validation RMSE = ',math.sqrt(MSE))
 
 print('[' + str(datetime.now()) + '] Finished!')
 sys.exit(0)
