811eb17d40195c77b9fa6dbc5fa138e92d2c800e
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2019-04-10 17:34:31 -0300

    Added multi-model capability to Keras trainer

--- trainer_keras.py
+++ trainer_keras.py
@@ -1,3 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Trains a Keras machine learning model from selected features.')
+parser.add_argument('modelname', metavar='modelname', type=str, help='Name for the model to be trained')
+parser.add_argument('featurelist', metavar='features', nargs='+', type=str, help='List of features used on model training')
+parser.add_argument('--list-features', action='store_true', default=False, dest='list_features', help='List available features')
+args = parser.parse_args()
+
 import numpy as np
 import pandas as pd
 import s3fs
@@ -5,7 +13,9 @@ import sys
 import os
 import tempfile
 import pickle
+import json
 import math
+import argparse
 from datetime import datetime
 from keras.models import Sequential
 from keras.layers import Dense
@@ -15,6 +25,67 @@ from keras import backend as K
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+print('[' + str(datetime.now()) + '] Reading available features file...')
+sys.stdout.flush()
+try:
+    #file = './data/OneHotEncodedDataset.parquet'                     # This line to read from local disk
+    features_file = 's3://w210policedata/datasets/AvailableFeatures.pickle'  # This line to read from S3
+    #training_data = pd.read_csv(file,sep=',', error_bad_lines=False, dtype='unicode')
+    s3 = s3fs.S3FileSystem(anon=False)
+    with s3.open(features_file, "rb") as json_file:
+        available_features = pickle.load(json_file)
+        json_file.close()
+except Exception as e:
+    print('[' + str(datetime.now()) + '] Error reading available features file: '+features_file)
+    print('[' + str(datetime.now()) + '] Error message: '+str(e))
+    print('[' + str(datetime.now()) + '] Aborting...')
+    sys.exit(1)
+
+if args.list_features:
+    print('[' + str(datetime.now()) + '] Available features: ')
+    for feature in available_features:
+        if feature['ethnically_biased']:
+            biased = 'Ethnically Biased'
+        else:
+            biased = 'Ethnically Unbiased'
+        if feature['optional']:
+            optional = 'Optional'
+        else:
+            optional = 'Required'
+        if feature['onehot-encoded']:
+            onehot = 'Categorical'
+        else:
+            onehot = 'Numerical'
+        print('[' + str(datetime.now()) + ']    * '+feature['feature']+' ('+optional+', '+onehot+', '+biased+')')
+    print('[' + str(datetime.now()) + '] Exiting...')
+    sys.exit(0)
+
+modelname = args.modelname
+featurelist = args.featurelist
+columns = []
+selected_features = []
+for feature in featurelist:
+    exists = False
+    for f in available_features:
+        if f['feature'] == feature:
+            exists = True
+            if not (f['column'] in columns):
+                columns.append(f['column'])
+                selected_features.append(f)
+            break
+        if f['required'] and not (f['column'] in columns):
+            columns.append(f['column'])
+            selected_features.append(f)
+    if not exists:
+        print('[' + str(datetime.now()) + '] Selected feature '+feature+' is not a valid available feature.')
+        print('[' + str(datetime.now()) + '] Aborting...')
+        sys.exit(1)
+
+print('[' + str(datetime.now()) + '] Training model '+modelname+' with features:')
+for feature in featurelist:
+    print('[' + str(datetime.now()) + ']    * '+feature)
+sys.stdout.flush()
 
 print('[' + str(datetime.now()) + '] Reading training dataset...')
 sys.stdout.flush()
@@ -66,6 +137,12 @@ print('[' + str(datetime.now()) + '] Training model...')
 sys.stdout.flush()
 df_Y = training_data.iloc[:,0]
 df_X = training_data.iloc[:,1:]
+# Pick only selected features for df_X
+regex=""
+for col in columns:
+    regex += '('+col+')|'
+regex = regex[:-1]
+df_X = df_X.filter(regex=regex,axis=1)
 train_X, test_X, train_y, test_y = train_test_split(df_X, df_Y, test_size = 0.10)
 y_scaler = MinMaxScaler()
 y_scaler.fit(train_y.values.reshape(-1,1))
@@ -92,38 +169,6 @@ model.fit(train_X, train_y, epochs=500, batch_size=32768, validation_split = 0.2
 print('[' + str(datetime.now()) + '] Reloading best model checkpoint...')
 model.load_weights("bestweights.hdf5")
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_absolute_error'])
-print('[' + str(datetime.now()) + '] Persisting model structure...')
-sys.stdout.flush()
-try:
-    s3 = s3fs.S3FileSystem(anon=False)
-    struct_file = "w210policedata/models/keras_struct.json"
-    with s3.open(struct_file, "w") as json_file:
-        json_file.write(model.to_json())
-        json_file.close()
-    print('[' + str(datetime.now()) + '] Persisting model weights...')
-    sys.stdout.flush()
-    temp_file = tempfile.NamedTemporaryFile(delete=True)
-    weights_file = "w210policedata/models/keras_weights.h5"
-    model.save_weights(temp_file.name)
-    s3.put(temp_file.name,weights_file)
-    temp_file.close()
-    print('[' + str(datetime.now()) + '] Persisting model features...')
-    sys.stdout.flush()
-    features = df_X.columns.values.tolist()
-    features_file = "w210policedata/models/keras_features.pickle"
-    with s3.open(features_file, "wb") as json_file:
-        pickle.dump(features, json_file, protocol=pickle.HIGHEST_PROTOCOL)
-        json_file.close()
-    print('[' + str(datetime.now()) + '] Persisting model scalers...')
-    sys.stdout.flush()
-    scaler_file = "w210policedata/models/keras_scaler.pickle"
-    with s3.open(scaler_file, "wb") as json_file:
-        pickle.dump(scaler, json_file, protocol=pickle.HIGHEST_PROTOCOL)
-        json_file.close()
-except:
-    print('[' + str(datetime.now()) + '] Error persisting model assets.')
-    print('[' + str(datetime.now()) + '] Aborting...')
-    sys.exit(1)
 ### END OF KERAS DEEP NEURAL NET MODEL
 
 ### MODEL TESTING
@@ -139,6 +184,48 @@ MSE = mean_squared_error(test_y, predictions)
 print('[' + str(datetime.now()) + ']    - Keras Deep Neural Net validation MAE = ',MAE)
 print('[' + str(datetime.now()) + ']    - Keras Deep Neural Net validation MSE = ',MSE)
 print('[' + str(datetime.now()) + ']    - Keras Deep Neural Net validation RMSE = ',math.sqrt(MSE))
+### END MODEL TESTING
+
+### PERSIST MODEL
+print('[' + str(datetime.now()) + '] Persisting model structure...')
+sys.stdout.flush()
+try:
+    s3 = s3fs.S3FileSystem(anon=False)
+    struct_file = "w210policedata/models/"+modelname+"/keras_struct.json"
+    with s3.open(struct_file, "w") as json_file:
+        json_file.write(model.to_json())
+        json_file.close()
+    print('[' + str(datetime.now()) + '] Persisting model weights...')
+    sys.stdout.flush()
+    temp_file = tempfile.NamedTemporaryFile(delete=True)
+    weights_file = "w210policedata/models/"+modelname+"/keras_weights.h5"
+    model.save_weights(temp_file.name)
+    s3.put(temp_file.name,weights_file)
+    temp_file.close()
+    print('[' + str(datetime.now()) + '] Persisting model features...')
+    sys.stdout.flush()
+    features = df_X.columns.values.tolist()
+    features_file = "w210policedata/models/"+modelname+"/keras_features.pickle"
+    with s3.open(features_file, "wb") as json_file:
+        pickle.dump(features, json_file, protocol=pickle.HIGHEST_PROTOCOL)
+        json_file.close()
+    print('[' + str(datetime.now()) + '] Persisting model scalers...')
+    sys.stdout.flush()
+    scaler_file = "w210policedata/models/"+modelname+"/keras_scaler.pickle"
+    with s3.open(scaler_file, "wb") as json_file:
+        pickle.dump(scaler, json_file, protocol=pickle.HIGHEST_PROTOCOL)
+        json_file.close()
+    print('[' + str(datetime.now()) + '] Persisting model information...')
+    modelinfo = {'modelname': modelname,'type':'keras','features':selected_features,'statistics':{'MAE':MAE,'MSE':MSE,'RMSE':math.sqrt(MSE)}}
+    info_file = "w210policedata/models/"+modelname+"/modelinfo.pickle"
+    with s3.open(info_file, "wb") as json_file:
+        pickle.dump(modelinfo, json_file, protocol=pickle.HIGHEST_PROTOCOL)
+        json_file.close()
+except:
+    print('[' + str(datetime.now()) + '] Error persisting model assets.')
+    print('[' + str(datetime.now()) + '] Aborting...')
+    sys.exit(1)
+### END OF MODEL PERSIST
 
 print('[' + str(datetime.now()) + '] Finished!')
 sys.exit(0)
