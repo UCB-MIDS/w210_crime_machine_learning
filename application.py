b95c22045e7b40d3275f9fca25dd6673a0ec6b5e
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2019-04-13 23:20:56 -0300

    Changes for further testing

--- application.py
+++ application.py
@@ -203,16 +203,16 @@ except:
 default_model = config['GENERAL']['DefaultModel']
 
 model,model_name,model_features,graph,model_type,model_scalers,model_info = load_model(default_model)
-#try:
-#    features_file = 's3://w210policedata/datasets/AvailableFeatures.pickle'
-#    s3 = s3fs.S3FileSystem(anon=False)
-#    with s3.open(features_file, "rb") as json_file:
-#        available_features = pickle.load(json_file)
-#        json_file.close()
+try:
+    features_file = 's3://w210policedata/datasets/AvailableFeatures.pickle'
+    s3 = s3fs.S3FileSystem(anon=False)
+    with s3.open(features_file, "rb") as json_file:
+        available_features = pickle.load(json_file)
+        json_file.close()
 #    features_file = 's3://w210policedata/datasets/AdditionalFeatures.parquet'
 #    features_data = pd.read_parquet(features_file)
-#except Exception as e:
-#    print('Failure reading additional feature data from S3.')
+except Exception as e:
+    print('Failure reading additional feature data from S3.')
 
 # Services to implement:
 #   * Train
@@ -485,8 +485,8 @@ class reloadModel(Resource):
             with s3.open(features_file, "rb") as json_file:
                 available_features = pickle.load(json_file)
                 json_file.close()
-            features_file = 's3://w210policedata/datasets/AdditionalFeatures.parquet'
-            features_data = pd.read_parquet(features_file)
+            #features_file = 's3://w210policedata/datasets/AdditionalFeatures.parquet'
+            #features_data = pd.read_parquet(features_file)
             return{'message':'Model loaded succesfully.','error':None,'result': 'success'}
         except Exception as e:
             return{'message':'Model load failed.','error':str(e),'result': 'failed'}
@@ -512,6 +512,7 @@ class getAvailableModels(Resource):
 class getAvailableFeatures(Resource):
     def get(self):
         global available_features
+        global features_data
         try:
             #file = './data/OneHotEncodedDataset.parquet'                     # This line to read from local disk
             features_file = 's3://w210policedata/datasets/AvailableFeatures.pickle'  # This line to read from S3
@@ -520,8 +521,8 @@ class getAvailableFeatures(Resource):
             with s3.open(features_file, "rb") as json_file:
                 available_features = pickle.load(json_file)
                 json_file.close()
-            features_file = 's3://w210policedata/datasets/AdditionalFeatures.parquet'
-            features_data = pd.read_parquet(features_file)
+            #features_file = 's3://w210policedata/datasets/AdditionalFeatures.parquet'
+            #features_data = pd.read_parquet(features_file)
         except Exception as e:
             return{'message':'Failure reading available features data from S3.','error':str(e),'result':'failed'}
         return {'features':available_features,'result':'success'}
